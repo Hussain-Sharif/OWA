638bba9d72cc0c0cb8af6590f696e0aba34fbdcd
hussain-sharif <99sharif00@gmail.com>
2025-09-10 23:40:24 +0530

    Made Github Commit Req Func+Tested

--- my-shit-app/src/index.ts
+++ my-shit-app/src/index.ts
@@ -1,9 +1,43 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { getGithubRepoInfo } from './routes/getGithubRepoInfo'
+import { Bindings, GithubBadResponseType, GithubGoodResponseType } from './libs/types'
+import { StatusCode } from 'hono/utils/http-status'
 
-const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+//CONFIG AND APP readiness
+const app = new Hono<{Bindings:Bindings}>()
+
+
+// CORS 
+app.use('/*', cors())
+
+
+//Routes 
+app.get('/', async(c) => {
+  // console.log('Environment check:', {
+  //   username: c.env.SHARIF_USERNAME,
+  //   reponame: c.env.SHARIF_REPONAME,
+  //   pat: c.env.SHARIF_PAT ? 'SET' : 'UNDEFINED'
+  // })
+  // return c.json({
+  //   username: !!c.env.SHARIF_USERNAME,
+  //   reponame: !!c.env.SHARIF_REPONAME,
+  //   pat: !!c.env.SHARIF_PAT
+  // })
+  console.log(c.env)
+  const response:GithubGoodResponseType|GithubBadResponseType=await getGithubRepoInfo(c.env.SHARIF_USERNAME,c.env.SHARIF_REPONAME,c.env.SHARIF_PAT)
+  c.status(response.statusCode as StatusCode)
+  return c.json({response})
+})
+
+app.notFound((c)=>{
+  return c.text('Not Found for the path '+c.req.path)
+})
+
+app.onError((error,c)=>{
+  c.status(500)
+  return c.text('Error for the path '+c.req.path+ ' Error Msg: '+error.message)
 })
 
 export default app
