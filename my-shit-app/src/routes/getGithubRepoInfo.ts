a0f2eacf15ba0acd0a89ce65d8dd799eea2c6c80
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-09-11 00:08:13 +0530

    It's biome Baby

--- my-shit-app/src/routes/getGithubRepoInfo.ts
+++ my-shit-app/src/routes/getGithubRepoInfo.ts
@@ -1,33 +1,32 @@
+export const getGithubRepoInfo = async (userName: string, repoName: string, pat: string) => {
+    try {
+        const urlPath = `https://api.github.com/repos/${userName}/${repoName}/commits`;
+        const response = await fetch(urlPath, {
+            headers: {
+                "User-Agent": "Hono-App",
+                Authorization: `token ${pat}`,
+            },
+        });
 
-export const getGithubRepoInfo = async (userName: string,repoName: string, pat: string) => {
-  try {
-    const urlPath=`https://api.github.com/repos/${userName}/${repoName}/commits`;
-    const response = await fetch(urlPath, {
-      headers: {
-        'User-Agent': 'Hono-App',
-        'Authorization': `token ${pat}`, 
-      },
-    });
+        if (!response.ok) {
+            return {
+                statusCode: response.status,
+                message: await response.text(),
+            };
+        }
 
-    if (!response.ok) {
-      return {
-        statusCode: response.status,
-        message: await response.text(),
-      };
+        const data = await response.json();
+        // console.log(data);
+        return {
+            statusCode: response.status,
+            message: "All Good We Got Data",
+            data: data,
+        };
+    } catch (error: any) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            message: "Internal Server Error",
+        };
     }
-
-    const data = await response.json();
-    // console.log(data);
-    return {
-        statusCode:response.status,
-        message:"All Good We Got Data",
-        data:data
-    };
-  } catch (error: any) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      message: 'Internal Server Error',
-    };
-  }
 };
